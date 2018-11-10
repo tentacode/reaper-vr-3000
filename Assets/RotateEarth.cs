31f7669d519fe6d18ff86ada7ca35087ef4916be
Gabriel Pillet <contact@gabrielpillet.com>
2018-11-10 16:24:43 +0100

    Skybox

---
+++ Assets/RotateEarth.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateEarth : MonoBehaviour
+{
+	Rigidbody rb;
+	public float torqueSpeed;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.AddTorque (transform.up * torqueSpeed);
+	}
+
+	void Update()
+	{
+		
+	}
+}
