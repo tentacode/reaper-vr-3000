f06d323031422b5663694e7d4c136eeb0ce96903
Plateau Julien <julien.plateau@gmail.com>
2018-11-10 17:48:18 +0100

    AJout du script editor pour les game events

---
+++ Assets/Scripts/Editor/EventEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[CustomEditor(typeof(GameEvent))]
+public class EventEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUI.enabled = Application.isPlaying;
+
+        GameEvent e = target as GameEvent;
+        if (GUILayout.Button("Raise"))
+            e.Raise();
+    }
+}
