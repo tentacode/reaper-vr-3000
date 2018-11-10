be7a6f9499af8bf00dfa025a6b5ef16e8faa8a1d
Plateau Julien <julien.plateau@gmail.com>
2018-11-10 17:23:35 +0100

    Ajout des game events

---
+++ Assets/Scripts/Events/GameEventListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class GameEventListener : MonoBehaviour
+{
+    [Tooltip("Event to register with.")]
+    public GameEvent Event;
+
+    [Tooltip("Response to invoke when Event is raised.")]
+    public UnityEvent Response;
+
+    public List<GameEvent> EventsAfter;
+
+    private void OnEnable()
+    {
+        Event.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        Event.UnregisterListener(this);
+    }
+
+    public void OnEventRaised()
+    {
+        Response.Invoke();
+
+        foreach(var e in EventsAfter)
+        {
+            e.Raise();
+        }
+    }
+}
+
