8f9532b3667c8235f06fc6bbcb8b6c101a6928f5
Gabriel Pillet <contact@gabrielpillet.com>
2018-11-11 15:08:23 +0100

    FINISH

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -6,23 +6,26 @@ using Valve.VR.InteractionSystem;
 public class Death : MonoBehaviour {
 
 	public GameObject particles;
-	public GameObject renderer;
 
 	// Use this for initialization
 	void Start () {
-		Invoke ("Die", 3.0f);
+		//Invoke ("Die", 3.0f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter(Collider collision)
+    {
+		if (collision.gameObject.tag == "Hand") {
+			Die();
+		}
+    }
 
 	void Die()
 	{
+		for (int i = 0; i < transform.childCount; i++) {
+			Destroy (transform.GetChild (i).gameObject);
+		}
+
 		GameObject blood = Instantiate (particles, transform);
 		GetComponent<PlaySound> ().Play();
-		renderer.SetActive (false);
 
 		Destroy (blood, 2.0f);
 		Destroy (gameObject, 2.0f);
