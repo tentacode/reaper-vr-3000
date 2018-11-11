2600e37a76fa0a0c27eeb68676c29d237c079a57
Gabriel Pillet <contact@gabrielpillet.com>
2018-11-11 11:18:11 +0100

    Reset VR

---
+++ Assets/Scripts/Controllers/ResetViewController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetViewController : MonoBehaviour
+{
+	public GameObject world;
+	public GameObject headset;
+	public float distanceZ;
+	public float distanceY;
+
+	public void ResetView()
+	{
+		var direction = headset.transform.forward;
+		Vector3 destination = (direction * distanceZ) + headset.transform.position;
+		destination.y = headset.transform.position.y + distanceY;
+
+		world.transform.position = destination;
+	}
+}
